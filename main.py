43b9d09f07e22c79dbd2b0da5b23a87120597114
DevCICDSatya <114742994+DevCICDSatya@users.noreply.github.com>
2022-11-29 17:02:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import altair as alt
 import streamlit as st
 import os
-os.system('!pip install plotly-express')
+os.system('!pip3 install plotly-express')
 import plotly.express as px
 import plotly.graph_objects as go
 
